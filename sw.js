044ed2513269fab6622381b776e469abfb48916d
Kepeh <252720290+kaffahstorage-stack@users.noreply.github.com>
2026-09-16 17:12:50 +0800

    Refresh offline app cache for static trading rules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX='tabungan:'+self.registration.scope+':';
-const CACHE=PREFIX+'v2';
+const CACHE=PREFIX+'v3';
 const FILES=['./','./index.html','./style.css','./app.js','./core.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
